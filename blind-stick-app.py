2a7fcee7cd5e9e66556f6f3a92b2e49408b6dc6a
Indra Putra <indrakoslab@gmail.com>
2018-10-07 17:48:49 +0700

    change library to os

--- blind-stick-app.py
+++ blind-stick-app.py
@@ -5,9 +5,6 @@ import os
 import RPi.GPIO as GPIO
 import time
 
-# import pyaudio
-# import wave
-
 # GPIO Mode (Board / BCM)
 GPIO.setmode(GPIO.BCM)
 
@@ -18,36 +15,6 @@ GPIO_ECHO = 21
 # GPIO setup
 GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
 GPIO.setup(GPIO_ECHO, GPIO.IN)
-
-# Define stream chunk
-chunk = 1024
-
-# setup for pyaudio file
-# halfMeter = wave.open(r"./voice/setengahmeter.wav", "rb")
-# oneMeter = wave.open(r"./voice/satumeter.wav", "rb")
-# twoMeter = wave.open(r"./voice/duameter.wav", "rb")
-# p = pyaudio.PyAudio()
-
-# streamHalf = p.open(format=p.get_format_from_width(halfMeter.getsampwidth()),
-#             channels=halfMeter.getnchannels(),
-#             rate=halfMeter.getframerate(),
-#             output=True)
-
-# streamOne = p.open(format=p.get_format_from_width(oneMeter.getsampwidth()),
-#             channels=oneMeter.getnchannels(),
-#             rate=oneMeter.getframerate(),
-#             output=True)
-
-# streamTwo = p.open(format=p.get_format_from_width(twoMeter.getsampwidth()),
-#         channels=twoMeter.getnchannels(),
-#         rate=twoMeter.getframerate(),
-#         output=True)
-
-
-# dataHalf = halfMeter.readframes(chunk)
-# dataOne = oneMeter.readframes(chunk)
-# dataTwo = twoMeter.readframes(chunk)
-
 
 def distance():
   # set Trigger to HIGH
@@ -73,28 +40,26 @@ def distance():
 
   return distance
 
-
 if __name__=='__main__':
   try:
     while True:
 
       dist = distance()
       print("Jarak Terukur = %.1f cm" % dist)
-      # time.sleep(1)
-      if dist > 3 and dist <= 5:
+
+      # voice control
+      if dist <= 5:
         print("setengah meter didepan")
-        # os.system("aplay ./voice/setengahmeter.wav &")
-
-      if dist > 6 and dist <= 10:
+        os.system("aplay ./voice/setengahmeter.wav &")
+        time.sleep(3)
+      if dist > 9  and dist <= 10:
         print("satu meter didepan")
-        # os.system("aplay ./voice/satumeter.wav &")
-
-      if dist > 11 and dist <= 20:
+        os.system("aplay ./voice/satumeter.wav &")
+        time.sleep(3)
+      if dist > 19 and dist <= 20:
         print("dua meter didepan")
-        # os.system("aplay ./voice/duameter.wav &")
-      sleep(0.1)
-      else:
-        print("oke")
+        os.system("aplay ./voice/duameter.wav &")
+        time.sleep(3)
 
   except KeyboardInterrupt:
     print("Monitoring stopped by user")
