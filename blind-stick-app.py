7d290a7e8fd6ab924389e2939bbea93a1900ef0a
Indra Putra <indrakoslab@gmail.com>
2018-10-07 14:46:35 +0700

    add time delay to access voices

--- blind-stick-app.py
+++ blind-stick-app.py
@@ -89,32 +89,32 @@ if __name__=='__main__':
         if dist < 5:
           streamHalf.write(dataHalf)
           dataHalf = halfMeter.readframes(chunk)
+          time.sleep(3)
+          streamHalf.stop_stream()
+          streamHalf.close()
+          p.terminate()
           # print("hati-hati didepan setengah meter")
         elif dist < 10:
           streamOne.write(dataOne)
           dataOne = oneMeter.readframes(chunk)
+          time.sleep(3)
+          streamOne.stop_stream()
+          streamOne.close()
+          p.terminate()
+
           # print("hati-hati didepan satu meter")
         elif dist < 20:
           streamTwo.write(dataTwo)
           dataTwo = twoMeter.readframes(chunk)
+          time.sleep(3)
+          streamTwo.stop_stream()
+          streamTwo.close()
+          p.terminate()          
           # print("hati-hati didepan dua meter")
       else:
         print("aman")
 
-
-      # control to voice
-
   
   except KeyboardInterrupt:
-    streamHalf.stop_stream()
-    streamHalf.close()
-
-    streamOne.stop_stream()
-    streamOne.close()
-
-    streamTwo.stop_stream()
-    streamTwo.close()
-
-    p.terminate()
     print("Monitoring stopped by user")
     GPIO.cleanup()
